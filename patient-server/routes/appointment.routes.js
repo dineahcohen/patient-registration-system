06bb720c485f7204838df2b360ddd7f0d6de9d63
dineahcohen <dineahcohen1@gmail.com>
2021-07-21 22:51:29 -0500

    created appointment model

---
+++ patient-server/routes/appointment.routes.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const appointmentSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    purpose: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: String,
+        required: true
+    },
+    time: {
+        type: String,
+        required: true
+    }
+}, {
+    collection: 'apppointments'
+})
+
+module.exports = mongoose.model('Appointment', appointmentSchema);
---
+++ patient-server/routes/users.route.js
@@ -0,0 +1,1 @@
+//Allow users to delete their accounts
