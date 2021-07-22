7301668f9417efea6946d412b762ccb6d24646cc
dineahcohen <dineahcohen1@gmail.com>
2021-07-21 19:40:24 -0500

    refactored db

--- patient-server/config/dB.js
+++ patient-server/config/dB.js
@@ -1,20 +1,18 @@
-//Database config
-
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        const conekt = await mongoose.connect(process.env.MONGO_URL,
-            {
-                useNewUrlParser: true,
-                useCreateIndex: true,
-                useUnifiedTopology: true
-            });
-        console.log(`MongoDB connected ${conekt.connection.host}`);
-    } catch {
-        console.log(`Error ${err.message}`);
+        const conct = await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useCreateIndex: true,
+            useUnifiedTopology: true
+        });
+
+        console.log(`MongoDB Connected: ${conct.connection.host}`);
+    } catch (err) {
+        console.log(`Error: ${err.message}`);
         process.exit(1);
     }
 }
 
-module.export = connectDB;
+module.exports = connectDB;
