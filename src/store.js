f911fc915d82115d2367868406c179440e664ea1
dineahcohen <dineahcohen1@gmail.com>
2021-07-27 18:35:37 -0500

    added user reducers

--- src/store.js
+++ src/store.js
@@ -1,18 +1,20 @@
-import { createStoreHook, combineReducers, applyMiddleware } from "redux";
+import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { userLoginReducer } from "./reducers/userReducers";
 
 const reducer = combineReducers({
-    //will container reducers
+    userLogin: userLoginReducer,
 });
 
 const initialState = {};
 
-const middleware = [think];
+const middleware = [thunk];
 
 const store = createStore(
     reducer,
     initialState,
-    applyMiddleware(...middleware)
+    composeWithDevTools(applyMiddleware(...middleware))
 );
 
 export default store;
