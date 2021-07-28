9547c0d790954df57acb19ef52fe415d38902f64
dineahcohen <dineahcohen1@gmail.com>
2021-07-27 21:27:09 -0500

    added user registration actions and added it to store

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -1,4 +1,4 @@
-import { USER_LOGIN_FAIL, USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS } from "../constants/userConstant"
+import { USER_LOGIN_FAIL, USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS, USER_REGISTER_FAIL, USER_REGISTER_REQUEST, USER_REGISTER_SUCCESS } from "../constants/userConstant"
 import axios from 'axios';
 
 export const login = (email, password) => async (dispatch) => {
@@ -28,3 +28,27 @@ export const login = (email, password) => async (dispatch) => {
         })
     }
 }
+
+export const register = (firstName, lastName, gender, email, address, phone, password) => async (dispatch) => {
+    try {
+        dispatch({ type: USER_REGISTER_REQUEST });
+
+        const user = { firstName, lastName, gender, email, password, address, phone }
+
+        const { data } = await axios.post('http://localhost:5000/api/auth/register', user);
+
+        dispatch({ type: USER_REGISTER_SUCCESS, payload: data });
+
+        dispatch({ type: USER_LOGIN_SUCCESS, payload: data });
+
+        localStorage.setItem('userInfo', JSON.stringify(data));
+    } catch (error) {
+        dispatch({
+            type: USER_REGISTER_FAIL,
+            payload:
+                error.response && error.response.data.message
+                    ? error.response.data.message
+                    : error.message
+        });
+    }
+};
